a377dc31366ad83843ee4d35a4d922e5549df161
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-09-27 21:25:43 +0300

    tagcloud

--- okgen/okgen/okgen_cms/views.py
+++ okgen/okgen/okgen_cms/views.py
@@ -36,11 +36,11 @@ def landing(request):
                 count >= 0 and (counts.append(count), taglist.append(tag))
             maxcount = max(counts)
             mincount = min(counts)
-            constant = log(maxcount - (mincount - 1))/(22 - 8 or 1)
+            constant = log(maxcount - (mincount - 1))/(22 - 10 or 1)
             tagcount = zip(taglist, counts)
             for tag, count in tagcount:
                 try:
-                    size = log(count - (mincount - 1))/constant + 8
+                    size = log(count - (mincount - 1))/constant + 10
                 except ZeroDivisionError:
                     size = 7
                     pass
