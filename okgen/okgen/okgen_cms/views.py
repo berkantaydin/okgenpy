6b3b8184b0f6b43707a26ae28e2c3f70ea4bcbcb
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-09-27 18:58:25 +0300

    tagcloud

--- okgen/okgen/okgen_cms/views.py
+++ okgen/okgen/okgen_cms/views.py
@@ -32,9 +32,25 @@ def landing(request):
         return render(request, 'okgen_cms/results.html', dict(lang=lang));
     else:
         links = Links.objects.filter(hidden=False).all()
+        max_clicked = 1
+        min_clicked = 1000000
+        for link in links:
+            max_clicked = link.clicked > max_clicked if link.clicked else max_clicked
+            min_clicked = link.clicked < min_clicked if link.clicked else min_clicked
 
         counts, taglist, tagcloud = [], [], []
         tags = Links.objects.filter(hidden=False).all()
+        if len(tags) > 1:
+            for tag in tags:
+                count = tag.clicked
+                count >= 0 and (counts.append(count), taglist.append(tag))
+            maxcount = max(counts)
+            mincount = min(counts)
+            constant = log(maxcount - (mincount - 1))/(22 - 8 or 1)
+            tagcount = zip(taglist, counts)
+            for tag, count in tagcount:
+                size = log(count - (mincount - 1))/constant + 8
+                tagcloud.append({'tag': tag, 'count': count, 'size': round(size, 7)})
 
         words = Words.objects.filter(hidden=False).order_by('-viewed').all()[:50]
         return render(request, 'okgen_cms/landing.html', dict(
