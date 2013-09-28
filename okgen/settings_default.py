1e2f03cb304cae6ba45a6de73a0683918a307b50
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-09-29 02:15:36 +0300

    debug mode

--- okgen/settings_default.py
+++ okgen/settings_default.py
@@ -70,11 +70,11 @@ MEDIA_URL = "/uploads/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticus")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
+STATIC_URL = "/staticus/"
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static'),
