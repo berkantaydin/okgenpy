6f8452edcfb55317a410592c7166f6e7bec3beb4
root <root@okgen-py.(none)>
2013-09-30 08:43:40 +0000

    fail resolved

--- okgen/settings_default.py
+++ okgen/settings_default.py
@@ -32,7 +32,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -200,8 +200,6 @@ LOGGING = {
         },
     }
 }
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 LANGUAGES = (
     ('tr', _('Turkish')),
