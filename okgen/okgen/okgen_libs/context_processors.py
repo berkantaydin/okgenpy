16a6a34cca63375e9f405242a38fab8d9d058d0a
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-01 19:51:59 +0300

    adstags

--- okgen/okgen/okgen_libs/context_processors.py
+++ okgen/okgen/okgen_libs/context_processors.py
@@ -5,4 +5,5 @@ def ads(request):
     return {
         "ads160x600": render_to_string('ads/160x600.html'),
         "ads728x90": render_to_string('ads/728x90.html'),
+        "ads336x280": render_to_string('ads/336x280.html'),
     }
