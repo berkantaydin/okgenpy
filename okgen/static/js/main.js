5683092b0406148de20d79f063cf4fafda6a0e0a
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-05 01:31:47 +0300

    currency

--- okgen/static/js/main.js
+++ okgen/static/js/main.js
@@ -7,4 +7,27 @@ $(function () {
             $("#weather").text(data.name + ' ' + parseInt(data.main.temp - 273.15) + ' C')
         }
     });
+
+    $.ajax({
+        url: "http://rate-exchange.appspot.com/currency?from=USD&to=TRY&q=1",
+        type: 'GET',
+        dataType: 'JSONP',
+        success: function (data) {
+            console.log(data);
+            $("#currency_dolar").text('$' + ' ' + data.rate.toFixed(2) + ' TL');
+        }
+    });
+
+    $.ajax({
+        url: "http://rate-exchange.appspot.com/currency?from=EUR&to=TRY&q=1",
+        type: 'GET',
+        dataType: 'JSONP',
+        success: function (data) {
+            console.log(data);
+            $("#currency_euro").text('€' + ' ' + data.rate.toFixed(2) + ' TL');
+        }
+    });
+
+
+
 });
