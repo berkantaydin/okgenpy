b90be5ce8b25f5cf3af60dd57ee17a5beb420d12
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-05 16:06:28 +0300

    ruya

--- okgen/okgen/okgen_dreams/views.py
+++ okgen/okgen/okgen_dreams/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from .models import Dreams
 
 
 def dreams(request):
     dreams = Dreams.objects.all()
+
+    paginator = Paginator(dreams, 50)
+
+    try:
+        dreams = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        dreams = paginator.page(paginator.num_pages)
     return render(request, 'okgen_dreams/dreams.html', dict(dreams=dreams))
 
 
