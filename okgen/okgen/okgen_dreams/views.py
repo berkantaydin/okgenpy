0be8704be6eb4e1da4ab0178b0acf082b39ee2f4
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-05 16:07:55 +0300

    ruya

--- okgen/okgen/okgen_dreams/views.py
+++ okgen/okgen/okgen_dreams/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from .models import Dreams
 
 
-def dreams(request):
+def dreams(request, page):
     dreams = Dreams.objects.all()
 
     paginator = Paginator(dreams, 50)
