4d690194eb96994edd8ec9ec62917c02599ac25a
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-06 16:26:19 +0300

    urls

--- okgen/okgen/okgen_dreams/urls.py
+++ okgen/okgen/okgen_dreams/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-                       url(r'^links/ruya-tabirleri/$', 'okgen_dreams.views.dreams', name='dreams_dreams'),
+                       url(r'^links/ruya-tabirleri/(?P<page>[\d-]*)$', 'okgen_dreams.views.dreams', name='dreams_dreams'),
                        url(r'^ruya-tabiri/(?P<slug>[\w-]+)/$', 'okgen_dreams.views.dream', name='dreams_dream'),
 )
