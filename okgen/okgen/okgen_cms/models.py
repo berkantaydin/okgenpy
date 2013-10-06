dbbab9af8d903d732fddb2be15573f994134940d
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-06 22:42:28 +0300

    banks and dreams on cms

--- okgen/okgen/okgen_cms/models.py
+++ okgen/okgen/okgen_cms/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from django_extensions.db.fields import AutoSlugField
 from taggit.managers import TaggableManager
-
+from okgen.okgen.okgen_banks.models import Banks
+from okgen.okgen.okgen_dreams.models import Dreams
 
 class Words(models.Model):
     word = models.CharField(max_length=255)
@@ -22,9 +23,17 @@ class Categories(MPTTModel):
         order_insertion_by = ['name']
 
     def children_count(self):
+        if self.slug == 'bankalar':
+            return Banks.objects.count()
+        if self.slug == 'ruya-tabirleri':
+            return Dreams.objects.count()
         return Links.objects.filter(category=self).count()
 
     def children_five_random(self):
+        if self.slug == 'ruya-tabirleri':
+            return Dreams.objects.order_by('?').all()[:5]
+        if self.slug == 'bankalar':
+            return Banks.objects.order_by('?').all()[:5]
         return Links.objects.filter(category=self).order_by('?').all()[:5]
 
 
