c946580e7e8115847577ba622e5f8ae8d2a8e691
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-07 16:51:23 +0300

    styles

---
+++ okgen/okgen/okgen_accounts/forms.py
@@ -0,0 +1,81 @@
+from django import forms
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+class AvatarForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar']
+
+
+class UserPasswordForm(forms.ModelForm):
+    password = forms.CharField(_('Password'), widget=forms.PasswordInput, )
+    password_check = forms.CharField(_('Password (Again)'), widget=forms.PasswordInput, )
+
+    class Meta:
+        model = User
+        fields = ['password', 'password_check']
+
+    def clean(self):
+        """
+        password check
+        """
+        data = self.cleaned_data
+
+        if not data['password_check']:
+            raise forms.ValidationError(_("You must confirm your password"))
+        if data['password'] != data['password_check']:
+            raise forms.ValidationError(_("Your passwords do not match"))
+
+        return data
+
+
+class UserEmailForm(forms.ModelForm):
+    email = forms.EmailField(label=_('Email'), help_text='Must be valid for activation.', )
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        """
+        email is unique ?
+        """
+        data = self.cleaned_data
+
+        try:
+            User.objects.get(email=data['email'])
+        except User.DoesNotExist:
+            return data['email']
+        raise forms.ValidationError(_("This email is already taken"))
+
+
+class UserForm(UserPasswordForm, UserEmailForm):
+    username = forms.CharField(label=_('Username'), )
+    email = forms.EmailField(label=_('Email'), help_text='Must be valid for activation.', )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_check', 'email']
+
+    def clean_username(self):
+        """
+        username is unique ?
+        """
+        data = self.cleaned_data
+
+        try:
+            User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+            return data['username']
+        raise forms.ValidationError(_("This username is already taken"))
+
+
+class LoginForm(forms.ModelForm):
+    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput, )
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
