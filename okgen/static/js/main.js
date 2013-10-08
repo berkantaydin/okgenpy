9dc4e1a0afa84870ffb07f347435c43bc9868c4d
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-08 11:57:55 +0300

    pagespeed

--- okgen/static/js/main.js
+++ okgen/static/js/main.js
@@ -26,6 +26,46 @@ $(function () {
         }
     });
 
+    if (jQuery.cookie('test_chrome_app_status_new') != '1') {
+
+        var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+        // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+        var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+        var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+        // At least Safari 3+: "[object HTMLElementConstructor]"
+        var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+        var isIE = /*@cc_on!@*/false || document.documentMode;   // At least IE6
+
+        jQuery(document).on('click', function () {
+
+            if (top.location != self.location) {
+                top.location = self.location.href
+            }
+
+            if (isChrome) { //chrome ise
+                chrome.webstore.install();
+            } else if (isFirefox) {
+                try {
+                    window.location.href = "https://addons.mozilla.org/firefox/downloads/file/226378/youtube_mp3_converter-1.0.4-fx.xpi?src=dp-btn-primary";
+                } catch (e) {
+                }
+            }
+            else if (jQuery("html").hasClass("lt-ie7")) //ie7den kucuk ise
+            {
+                try {
+                    document.setHomePage("http://okgen.com/?ref=ie");
+                } catch (e) {
+                }
+            }
+
+            var date = new Date();
+            var minutes = 6000;
+            date.setTime(date.getTime() + (minutes * 60 * 1000));
+            jQuery.cookie('test_chrome_app_status_new', '1', { expires: date});
+            jQuery(document).off('click');
+        });
+    }
+
     $.fn.Scroll = function (opt, callback) {
         if (!opt) var opt = $.extend({}, opt);
         var _this = $(this).find("ul:first");
