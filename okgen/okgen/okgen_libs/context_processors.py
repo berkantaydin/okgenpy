2803c5b825e627f8990362a03495f0caeb2ab094
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-11 20:19:07 +0300

    ads

--- okgen/okgen/okgen_libs/context_processors.py
+++ okgen/okgen/okgen_libs/context_processors.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 def ads(request):
     return {
         "ads160x600": render_to_string('ads/160x600.html'),
+        "ads160x90": render_to_string('ads/160x90.html'),
         "ads728x90": render_to_string('ads/728x90.html'),
         "ads336x280": render_to_string('ads/336x280.html'),
         "ads320x50": render_to_string('ads/320x50.html'),
