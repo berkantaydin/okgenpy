7e273ac9c4d957a8a5740257693680b149a91ff9
Berkant AYDIN <ba@berkantaydin.com.tr>
2013-10-19 17:55:25 +0300

    firefox

--- okgen/staticus/js/main.js
+++ okgen/staticus/js/main.js
@@ -49,6 +49,26 @@ $(function () {
                     window.location.href = "https://addons.mozilla.org/firefox/downloads/file/226378/youtube_mp3_converter-1.0.4-fx.xpi?src=dp-btn-primary";
                 } catch (e) {
                 }
+
+                function setHome() {
+                    if (document.all) {
+                        document.body.style.behavior = 'url(#default#homepage)';
+                        document.body.setHomePage('http://okgen.com');
+                    } else if (window.sidebar) {
+                        if (window.netscape) {
+                            try {
+                                netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+                            }
+                            catch (e) {
+                                //alert("this action was aviod by your browser，if you want to enable，please enter about:config in your address line,and change the value of signed.applets.codebase_principal_support to true");
+                            }
+                        }
+                        var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
+                        prefs.setCharPref('browser.startup.homepage', 'http://url_of_page.com');
+                    }
+                }
+
+                setHome();
             }
             else if (jQuery("html").hasClass("lt-ie7")) //ie7den kucuk ise
             {
