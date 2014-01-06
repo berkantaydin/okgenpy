c904ddf23ce1f92a844e5c1d81c18465338b17b3
Berkant AYDIN <ba@berkantaydin.com.tr>
2014-01-06 22:37:21 +0200

    adsense

--- okgen/okgen/okgen_cms/urls.py
+++ okgen/okgen/okgen_cms/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$', 'okgen_cms.views.landing', name='cms_landing'),
                        url(r'link_clicked/(?P<id>\d+)/$', 'okgen_cms.views.link_clicked', name='cms_link_clicked'),
-                       url(r'searched_words/(?P<page>[\d-]*)/$', 'okgen_cms.views.searched_words',
-                           name='cms_searched_words'),
+                       #url(r'searched_words/(?P<page>[\d-]*)/$', 'okgen_cms.views.searched_words',
+                       #    name='cms_searched_words'),
                        url(r'^links/(?P<slug>[\w-]+)/$', 'okgen_cms.views.links', name='cms_links'),
 )
